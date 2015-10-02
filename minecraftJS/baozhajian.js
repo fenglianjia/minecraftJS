a8b1050d92647b13d7610d08fa81a90a9e9cfd66
liyadong <liyadong@syswin.com>
2015-10-02 17:24:08 +0800

    选铁剑然后攻击地面，炸很深~~

---
+++ minecraftJS/baozhajian.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2015/10/2.
+ */
+function useItem(x,y,z,itemid,blockid){
+    if(itemid==267){
+        explode (getPlayerX(),getPlayerY(),getPlayerZ(),100)
+    }
+}
